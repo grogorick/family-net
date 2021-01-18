8d35a849df1bb2f52b9ad788ecb9f04aa31548f1
Steve Grogorick <steve@grogorick.de>
2021-01-18 08:53:28 +0100

    bugfix find Rufname with umlauts

--- script.js
+++ script.js
@@ -163,9 +163,9 @@ function getGraphPositionFromEvent(e)
 
 function getPersonRufname(d_n)
 {
-  let n = d_n.match(/[*]\s*(\w+)(,|\s|$)/);
+  let n = d_n.match(/[*]\s*([^ ,?]+)(,|\s|$)/);
   if (!n) {
-    n = d_n.match(/^(\w+)(,|\s|$)/);
+    n = d_n.match(/^([^ ,?]+)(,|\s|$)/);
   }
   return n ? n[1] : '';
 }
